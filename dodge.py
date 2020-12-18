df3a2acce6ea38b2b4ac11af280234163deb774b
EB-coder <73636880+EB-coder@users.noreply.github.com>
2020-12-18 22:43:07 +0600

    Update dodge.py

--- dodge.py
+++ dodge.py
@@ -136,3 +136,48 @@ def game_loop():
 		thing_height=100
 
 		crashed=False
+		while not crashed:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					quit()
+				i+=0.02
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_LEFT:
+						x_change=-(8+i)
+					if event.key == pygame.K_RIGHT:
+						x_change=8+i
+					if event.key == pygame.K_p:
+						paused()
+				if event.type ==pygame.KEYUP:
+					if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+						x_change=0
+
+			x+=x_change
+			gameDisplay.fill(white)
+			things(thing_startx,thing_starty,thing_width,thing_height,black)
+			thing_starty+=thing_speed
+			car(x,y)
+			things_dodged(dodged)
+
+			if x > display_width-car_width or x<0:
+				crash()
+			
+			if thing_starty > display_height:
+				thing_starty=0-thing_height
+				thing_startx=random.randrange(0,display_width-thing_width)
+				dodged+=1
+				thing_speed+=0.33
+
+			if y<thing_starty+thing_height:
+				print("y crossover")
+				if x>thing_startx and x<thing_startx+thing_width or x+car_width>thing_startx and x+car_width<thing_startx+thing_width or x<thing_startx and x+car_width>thing_startx+thing_width: 
+					print("x crossover")
+					crash()
+
+			pygame.display.update()
+			clock.tick(70)
+
+game_intro()
+game_loop()
+pygame.quit()
